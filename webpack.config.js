975c14a245d9468d5d35baa504d0242ca5e7915c
Marketgai <marcusalee@gmail.com>
2020-11-14 20:02:41 -0800

    jest installed, react not tested

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const CLIENT_DIR = path.join(__dirname, '/client');
+const PUBLIC_DIR = path.join(__dirname, '/public');
+module.exports = {
+  mode: 'development',
+  entry: `${CLIENT_DIR}/index.jsx`,
+  output: {
+    path: PUBLIC_DIR,
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: CLIENT_DIR,
+        exclude: [/node_modules/],
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-react', '@babel/preset-env'],
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.jsx', '.css'],
+  },
+  watch: true,
+  optimization: {
+    minimize: false,
+  },
+};
