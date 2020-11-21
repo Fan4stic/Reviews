ff47a0d9339328a00440b8dd9dfc6587085422e5
Marketgai <marcusalee@gmail.com>
2020-11-20 23:47:25 -0800

    before modal changes

--- seed.js
+++ seed.js
@@ -1,85 +1,84 @@
 const faker = require('faker');
 const connection = require('./db/index.js');
-// const model = require('./db/model.js')
 
-//create fake review objects. then take them and add them to mySQL.
-let reviewsArray = [];
+let restaurantCount = 0;
+let userCount = 0;
+let reviewCount = 0;
+let photoCount = 0;
 
-createEntries = (amount) => {
-  for (i = 0; i <= amount; i++) {
-    let reviewObj = {};
-    reviewObj.rating = Math.floor(Math.random() * 6);
-    reviewObj.overallRating = (Math.random() * 5);
-    reviewObj.reviewText = faker.lorem.paragraphs(2);
-    reviewObj.reviewDate = faker.date.past().toJSON().slice(0, 10);
-    reviewObj.comment = faker.lorem.paragraph();
-    reviewObj.commentFrom = faker.name.findName();
-    reviewObj.commentCreatedAt = faker.date.past().toJSON().slice(0, 10);
-    reviewObj.resName = faker.lorem.word(2);
-    reviewObj.imgPath = faker.image.food();
-    reviewObj.elite = faker.random.boolean();
-    reviewObj.userName = faker.name.findName();
-    reviewObj.useful = Math.floor(Math.random() * 100);
-    reviewObj.helpful = Math.floor(Math.random() * 100);
-    reviewObj.cool = Math.floor(Math.random() * 100);
-    reviewObj.userID = Math.floor(Math.random() * 100);
-    reviewObj.resID = Math.floor(Math.random() * 100);
-    reviewObj.revID = Math.floor(Math.random() * 10000);
-    reviewObj.friends = Math.floor(Math.random() * 500);
-    reviewObj.reviewCount = Math.floor(Math.random() * 100);
-    reviewObj.city = faker.address.city();
-    reviewObj.userState = faker.address.state();
-    reviewObj.avatar = faker.image.avatar();
-
-
-    reviewsArray.push(reviewObj);
+createRestaurants = (amount) => {
+  while (restaurantCount < amount) {
+    let queryStr = `insert into Restaurants values (0, "${faker.lorem.word()}", ${Math.random() * 5})`;
+    connection.asyncQuery(queryStr);
+    restaurantCount++;
   }
 };
 
-//generate full restaurants table first, then users, then reviews, then photos.
-
-addEntries = (arrayOfReviews) => {
-  //form post query to model for each
-  for (k = 0; k < arrayOfReviews.length; k++) {
-
-    let userQuery = `insert into Users values(0, '${arrayOfReviews[k].userName}' , '${arrayOfReviews[k].friends}', '${arrayOfReviews[k].city}', '${arrayOfReviews[k].userState}', '${arrayOfReviews[k].reviewCount}', ${arrayOfReviews[k].elite}, '${arrayOfReviews[k].avatar}');`;
-    connection.query(userQuery, (err) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log('added user');
-      }
-    });
-
-    let restQuery = `insert into Restaurants values (0, '${arrayOfReviews[k].resName}', ${arrayOfReviews[k].overallRating});`;
-    connection.query(restQuery);
-
-
-    let queryStr = `insert into Reviews (rating, reviewText, reviewDate, comment, commentFrom, commentCreatedAt, useful, helpful, cool, userID, resID) values (${arrayOfReviews[k].rating}, '${arrayOfReviews[k].reviewText}', '${arrayOfReviews[k].reviewDate}', '${arrayOfReviews[k].comment}', "${arrayOfReviews[k].commentFrom}", '${arrayOfReviews[k].commentCreatedAt}', '${arrayOfReviews[k].useful}', '${arrayOfReviews[k].helpful}', '${arrayOfReviews[k].cool}', ${arrayOfReviews[k].userID}, ${arrayOfReviews[k].resID});`;
-    connection.query(queryStr, (err) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log('added to db');
-      }
-    });
-
-    let photoQuery = `insert into Photos (photoID, imgPath, revID) values (0, '${arrayOfReviews[k].imgPath}', ${arrayOfReviews[k].revID})`;
-    connection.query(photoQuery, (err) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log('added to photos');
-      }
-    });
+createUsers = (amount) => {
+  while (userCount < amount) {
+    let queryStr = `insert into Users values (0, "${faker.name.firstName()} ${faker.lorem.word(1).toUpperCase()}.", ${Math.floor(Math.random() * 500)}, "${faker.address.city()}", "${faker.address.stateAbbr()}", ${Math.floor(Math.random() * 100)}, ${Math.floor(Math.random() * 100)}, ${faker.random.boolean()}, "${faker.image.avatar()}")`;
+    connection.asyncQuery(queryStr);
+    userCount++;
   }
-  connection.end();
 };
 
-createEntries(50);
+createReviews = (amount, users, restaurants) => {
+  while (reviewCount < amount) {
+    let queryStr = `insert into Reviews values(0, ${Math.floor(Math.random() * 6)}, "${faker.lorem.paragraphs(3)}", ${Math.floor(Math.random() * users)}, ${Math.floor(Math.random() * restaurants)}, "${faker.date.past().toJSON().slice(0, 10)}", ${Math.floor(Math.random() * 100)}, ${Math.floor(Math.random() * 100)}, ${Math.floor(Math.random() * 100)}, "${faker.lorem.paragraph()}", "${faker.name.findName()}", "${faker.date.recent().toJSON().slice(0, 10)}")`;
+    connection.asyncQuery(queryStr);
+    reviewCount++;
+  }
+};
+
+createPhotos = (amount, reviews) => {
+  while (photoCount < amount) {
+    let queryStr = `insert into Photos values(0, ${Math.floor(Math.random() * reviews)}, "${faker.image.food()}")`;
+    connection.asyncQuery(queryStr);
+    photoCount++;
+  }
+};
+//reviews and photos dont work because foreign keys... they dont exist before theyre created. async issues.
+
+createRestaurants(100);
+createUsers(100);
+createReviews(10000, 100, 100);
+createPhotos(100, 100);
+
+connection.end();
+//need to run these commands manually to add foreign keys back
+
+// let addFKConstraints = (() => {
+//   let queryStr0 = 'set foriegn_key_checks=0';
+//   let queryStr1 = 'alter table Reviews add constraint fk_userID foreign key (userID) references Users(userID)';
+//   let queryStr2 = 'alter table Reviews add constraint fk_resID foreign key (resID) references Restaurants(resID)';
+//   let queryStr3 = 'alter table Photos add constraint fk_revID foreign key (revID) references Reviews(revID)';
+
+//   connection.query(queryStr0, (er, res1) => {
+//     if (er) {
+//       console.log(er)
+//     } else {
+//       connection.query(queryStr1, (er, res2) => {
+//         if (er) {
+//           console.log(er)
+//         } else {
+//           connection.query(queryStr2, (er, res3) => {
+//             if (er) {
+//               console.log(er)
+//             } else {
+//               connection.query(queryStr3)
+//     }
+//   }
+//   connection.query(queryStr1);
+//   connection.query(queryStr2);
+//   connection.query(queryStr3);
+// });
+
+// addFKConstraints();
 
 
 
-addEntries(reviewsArray);
+
+
+
 
 
