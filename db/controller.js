1a1551fbe98d378506c6909aa2ddbf5f621b3693
Marketgai <marcusalee@gmail.com>
2020-11-21 15:41:58 -0800

    edited query

--- db/controller.js
+++ db/controller.js
@@ -7,7 +7,7 @@ module.exports = {
     //still dont know how i would get all the photos for a particular review if there were more than 1, has issue if multiple photos, it will send duplicate
     //grab review IDs, query photos for associated photos, attach them to proper review, send response
     // let queryStr = `select * from restaurants inner join reviews using (resID) inner join users on reviews.userID = users.userID inner join photos on reviews.revID = photos.revID where resID = ${id} limit 10`;
-    let queryStr = `select * from restaurants inner join reviews using (resID) inner join users on reviews.userID = users.userID where resID = ${id} limit 35`
+    let queryStr = `select * from Restaurants inner join Reviews using (resID) inner join Users on Reviews.userID = Users.userID where resID = ${id} limit 35`
       try {
         const result = await connection.asyncQuery(queryStr);
         // console.log(result);
@@ -19,7 +19,7 @@ module.exports = {
   post: async (req, res) => {
     let id = req.url.slice(17, -8);
     //send userID from req.body
-    let queryStr = `insert into reviews (rating, reviewText, userID, resID, reviewDate) values (${req.body.rating}, "${req.body.reviewText}", ${req.body.userID}, ${id}, "${req.body.reviewDate}")`;
+    let queryStr = `insert into Reviews (rating, reviewText, userID, resID, reviewDate) values (${req.body.rating}, "${req.body.reviewText}", ${req.body.userID}, ${id}, "${req.body.reviewDate}")`;
     console.log(queryStr)
     try {
       const result = await connection.asyncQuery(queryStr);
