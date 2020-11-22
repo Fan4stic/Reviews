57eed1dc00ec888f4ae7a2c03a160dbc262961e0
Marketgai <marcusalee@gmail.com>
2020-11-21 18:29:55 -0800

    async await seed changes

--- seed.js
+++ seed.js
@@ -6,34 +6,34 @@ let userCount = 0;
 let reviewCount = 0;
 let photoCount = 0;
 
-createRestaurants = (amount) => {
+createRestaurants = async (amount) => {
   while (restaurantCount < amount) {
     let queryStr = `insert into Restaurants values (0, "${faker.lorem.word()}", ${Math.random() * 5})`;
-    connection.asyncQuery(queryStr);
+    await connection.asyncQuery(queryStr);
     restaurantCount++;
   }
 };
 
-createUsers = (amount) => {
+createUsers = async (amount) => {
   while (userCount < amount) {
     let queryStr = `insert into Users values (0, "${faker.name.firstName()} ${faker.lorem.word(1).toUpperCase()}.", ${Math.floor(Math.random() * 500)}, "${faker.address.city()}", "${faker.address.stateAbbr()}", ${Math.floor(Math.random() * 100)}, ${Math.floor(Math.random() * 100)}, ${faker.random.boolean()}, "${faker.image.avatar()}")`;
-    connection.asyncQuery(queryStr);
+    await connection.asyncQuery(queryStr);
     userCount++;
   }
 };
 
-createReviews = (amount, users, restaurants) => {
+createReviews = async (amount, users, restaurants) => {
   while (reviewCount < amount) {
     let queryStr = `insert into Reviews values(0, ${Math.floor(Math.random() * 6)}, "${faker.lorem.paragraphs(3)}", ${Math.floor(Math.random() * users)}, ${Math.floor(Math.random() * restaurants)}, "${faker.date.past().toJSON().slice(0, 10)}", ${Math.floor(Math.random() * 100)}, ${Math.floor(Math.random() * 100)}, ${Math.floor(Math.random() * 100)}, "${faker.lorem.paragraph()}", "${faker.name.findName()}", "${faker.date.recent().toJSON().slice(0, 10)}")`;
-    connection.asyncQuery(queryStr);
+    await connection.asyncQuery(queryStr);
     reviewCount++;
   }
 };
 
-createPhotos = (amount, reviews) => {
+createPhotos = async (amount, reviews) => {
   while (photoCount < amount) {
     let queryStr = `insert into Photos values(0, ${Math.floor(Math.random() * reviews)}, "${faker.image.food()}")`;
-    connection.asyncQuery(queryStr);
+    await connection.asyncQuery(queryStr);
     photoCount++;
   }
 };
