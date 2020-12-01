64a029ffb62d831e478b6936490470dde93b0e83
Marketgai <marcusalee@gmail.com>
2020-12-01 02:29:46 -0800

    if 0 photos, svg disappears

--- client/ListItem.jsx
+++ client/ListItem.jsx
@@ -175,10 +175,11 @@ const ListItem = (props) => (
       <img style={starStyle} src={ratingToStars(props.review.rating)}/>
       <span style={dateStyle}>{props.review.reviewDate.slice(5, 7) + '/' + props.review.reviewDate.slice(-2) + '/' + props.review.reviewDate.slice(0, 4)}
       </span>
-      <div className="photoText">
-        <svg style={photoSVG} xmlns="http://www.w3.org/2000/svg" width="16" height="16" className="iconsvg"><path d="M11.23 1A3.77 3.77 0 0115 4.77v6.46A3.77 3.77 0 0111.23 15H4.77a3.77 3.77 0 01-3.43-2.31l-.06-.06a3.61 3.61 0 01-.28-1.4V4.77A3.77 3.77 0 014.77 1h6.46zM3 4.77V11l5.73-5.44a1 1 0 011.38 0L13 8.3V4.77A1.77 1.77 0 0011.23 3H4.77A1.77 1.77 0 003 4.77zM11.23 13A1.77 1.77 0 0013 11.23v-.17l-3.58-3.4L4 12.81c.238.122.502.187.77.19h6.46zM5.02 6.83a1.2 1.2 0 110-2.4 1.2 1.2 0 010 2.4z"/></svg>
-        <span className="photoText">{props.review.numberPhotos} photos</span>
-      </div>
+      {props.review.numberPhotos > 0 ?
+        <div className="photoText">
+          <svg style={photoSVG} xmlns="http://www.w3.org/2000/svg" width="16" height="16" className="iconsvg"><path d="M11.23 1A3.77 3.77 0 0115 4.77v6.46A3.77 3.77 0 0111.23 15H4.77a3.77 3.77 0 01-3.43-2.31l-.06-.06a3.61 3.61 0 01-.28-1.4V4.77A3.77 3.77 0 014.77 1h6.46zM3 4.77V11l5.73-5.44a1 1 0 011.38 0L13 8.3V4.77A1.77 1.77 0 0011.23 3H4.77A1.77 1.77 0 003 4.77zM11.23 13A1.77 1.77 0 0013 11.23v-.17l-3.58-3.4L4 12.81c.238.122.502.187.77.19h6.46zM5.02 6.83a1.2 1.2 0 110-2.4 1.2 1.2 0 010 2.4z"/></svg>
+          <span className="photoText">{props.review.numberPhotos} photos</span>
+        </div> : <br></br> }
       <br></br>
       <div style={textStyle}> {props.review.reviewText}</div>
       <br></br>
